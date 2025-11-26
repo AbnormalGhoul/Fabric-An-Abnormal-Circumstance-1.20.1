35cf70ef2c3aaee6f58163b800dc8f542c0f81f9
Abnormal Ghoul <151715344+AbnormalGhoul@users.noreply.github.com>
2025-11-25 16:36:49 -0800

    Added A protection mixin for when I want to add prot 5 to the meta

---
+++ src/main/java/net/abnormal/anabnormalcircumstance/mixin/ProtectionEnchantmentMixin.java
@@ -0,0 +1,18 @@
+//package net.abnormal.anabnormalcircumstance.mixin;
+//
+//import net.minecraft.enchantment.ProtectionEnchantment;
+//import org.spongepowered.asm.mixin.Mixin;
+//import org.spongepowered.asm.mixin.injection.At;
+//import org.spongepowered.asm.mixin.injection.Inject;
+//import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+//
+//// For prot 5 meta
+//@Mixin(ProtectionEnchantment.class)
+//public class ProtectionEnchantmentMixin {
+//
+//    @Inject(method = "getMaxLevel", at = @At("HEAD"), cancellable = true)
+//    private void increaseMaxLevel(CallbackInfoReturnable<Integer> cir) {
+//        cir.setReturnValue(5);  // New Max Level
+//    }
+//}
+//
